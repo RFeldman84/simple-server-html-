b544d52a2f546898c73f2bb4d5f798637d96283c
RFeldman84 <rachel.feldman84@gmail.com>
2019-09-07 16:20:40 -0400

    went back to 053ce62dd206b9fd5d627b8149d91edd730bcef3

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-asdf;lkajsdf;laksdf
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.set(express.static(path.join(__dirname, "public")));
+
+const index = require("./routes/index");
+app.use("/", index);
+
+const port = 5000;
+app.listen(port, function() {
+	console.log(`simple server running on ${port}`);
+});
